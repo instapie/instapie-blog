184bc3f23d5001cc1922440cc8abd796a59dc9a9
Dan Tao <daniel.tao@gmail.com>
2013-08-05 13:43:44 -0700

    12

--- articulate.js
+++ articulate.js
@@ -420,6 +420,11 @@ function removeFromArray(array, element) {
 function isEditing() {
   var element = getCurrentElement();
 
+  // If no element is selected then obviously we're not editing.
+  if (!element) {
+    return false;
+  }
+
   // Special case -- don't treat focus on the article *itself* as editing.
   if (element.nodeName === 'ARTICLE') {
     return false;
